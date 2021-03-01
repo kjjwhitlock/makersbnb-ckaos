234457b03bfb7192b51954af6bc9e3e14e57cf36
Adam Jonas <adamjonas25@gmail.com>
2021-03-01 17:30:56 +0000

    tests for database connection model

---
+++ spec/models/database_connection_spec.rb
@@ -0,0 +1,16 @@
+describe DatabaseConnection do
+  describe '.setup' do
+    it 'calls connect on PG' do
+      expect(PG).to receive(:connect).with(dbname: 'makersbnb_test')
+      described_class.setup(dbname: 'makersbnb_test')
+    end
+  end
+
+  describe '.query' do
+    xit 'calls a query on the connection from .setup' do
+      connection = described_class.setup(dbname: 'makersbnb_test')
+      expect(connection).to receive(:exec).with('SELECT * FROM users;')
+      described_class.query('SELECT * FROM users;')
+    end
+  end
+end
